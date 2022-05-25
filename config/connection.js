9fe10c8ea6d8d5faa312ba937902b3524e9ac161
wilgru <william.gruszka@gmail.com>
2022-05-25 21:55:33 +1000

    init sequelize connection

--- config/connection.js
+++ config/connection.js
@@ -10,11 +10,11 @@ if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(
         process.env.DB_NAME,
         process.env.DB_USER,
-        process.env.DB_PASS,
+        process.env.DB_PASSWORD,
         {
             host: 'localhost',
             dialect: 'mysql',
-            port: 3001
+            port: 3306
         }
     );
 }

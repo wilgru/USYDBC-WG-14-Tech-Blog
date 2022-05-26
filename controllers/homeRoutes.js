0d95dec343430c0436368c5544985bb3c3116b4a
wilgru <william.gruszka@gmail.com>
2022-05-26 15:14:59 +1000

    added signup routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -9,23 +9,52 @@ router.get('/', async (req, res) => {
             include: [
                 {
                     model: User,
+                    attributes: ['id', 'name'],
+                },
+            ],
+        });
+    
+        // Serialize data so the template can read it
+        const postsi = postsData.map((post) => post.get({ plain: true }));
+        const posts = postsi.map((post) => {
+            post.isThisUser = (post.postedBy === req.session.user_id);
+            return post;
+        });
+    
+        // Pass serialized data and session flag into template
+        res.render('homepage', { posts, logged_in: req.session.logged_in })
+    } catch(error) {
+        res.status(500).send(error);
+    }
+});
+
+router.get('/dashboard', withAuth, async (req, res) => {
+    try {
+        // Get all posts and JOIN with user data
+        const postsData = await Post.findAll({
+            where: {
+                postedBy: req.session.user_id
+            },
+            include: [
+                {
+                    model: User,
                     attributes: ['name'],
                 },
             ],
         });
     
         // Serialize data so the template can read it
-        const posts = postsData.map((post) => post.get({ plain: true }));
+        const postsi = postsData.map((post) => post.get({ plain: true }));
+        const posts = postsi.map((post) => {
+            post.isThisUser = (post.postedBy === req.session.user_id);
+            return post;
+        });
     
         // Pass serialized data and session flag into template
-        res.render('homepage', { posts, loggedin: req.session.loggedin })
+        res.render('dashboard', { posts, logged_in: req.session.logged_in })
     } catch(error) {
         res.status(500).send(error);
-    }
-});
-
-router.get('/dashboard', withAuth, (req, res) => {
-    res.render('dashboard', {});
+    };
 });
 
 router.get('/post/:postid', async (req, res) => {
@@ -33,14 +62,30 @@ router.get('/post/:postid', async (req, res) => {
         where: {
             id: req.params.postid
         },
-        include: {
-            model: Comment
-        }
+        include: [
+            {
+              model: User,
+              attributes: ['id', 'name']
+            },
+            {
+              model: Comment,
+              attributes: ['body'],
+              include: [
+                  {
+                      model: User,
+                      attributes: ['name']
+                  }
+              ]
+            }
+        ]
     });
 
     const post = postData.get({ plain: true });
+    post.isThisUser = (post.postedBy === req.session.user_id);
 
-    res.render('post', { post });
+    console.log(post)
+
+    res.render('post', { post, logged_in: req.session.logged_in });
 });
 
 router.get('post/:postid/newcomment/:postid', withAuth, (req, res) => {
@@ -51,24 +96,28 @@ router.get('post/:postid/editcomment/:postid', withAuth, (req, res) => {
 
 });
 
-router.post('/newpost', withAuth, (req, res) => {
-
+router.get('/newpost', withAuth, (req, res) => {
+    res.render('newpost');
 });
 
-router.get('/editpost/:postid', withAuth, (req, res) => {
+router.get('/post/edit/:postid', withAuth, (req, res) => {
 
 });
 
 router.get('/login', (req, res) => {
-    if (req.session.loggedin) {
-        res.redirect('/');
+    if (req.session.logged_in) {
+        res.render('homepage');
     } else {
         res.render('login', {});
     }
 });
 
 router.get('/signup', (req, res) => {
-
+    if (req.session.logged_in) {
+        res.status(404).render('homepage');
+    } else {
+        res.render('signup');
+    }
 });
 
 router.get('*', (req, res) => {

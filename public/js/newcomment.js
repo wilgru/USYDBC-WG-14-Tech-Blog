1231c17278ac47230a526781f4661af285e23903
wilgru <william.gruszka@gmail.com>
2022-05-27 23:50:26 +1000

    added formHandler

---
+++ public/js/newcomment.js
@@ -0,0 +1,32 @@
+const form = document.getElementById('form-new-comment');
+
+const formHandler = async (event) => {
+    event.preventDefault();
+
+    // Collect values from the login form
+    const body = document.querySelector('#body-input');
+    const bodyVal = body.value.trim();
+
+    if (!bodyVal) {
+        body.style.backgroundColor = "lightcoral";
+        return;
+    } 
+
+    const urlItems = window.location.href.split('/')
+    const postId = urlItems[urlItems.length - 1]
+
+    const commentStatus = await fetch(`/api/comments/${postId}`, {
+        method: 'POST',
+        body: JSON.stringify({ body: bodyVal }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+
+    if (!commentStatus.ok) {
+        alert(commentStatus.statusText);
+        return;
+    } else {
+        window.location = `/post/view/${postId}`;
+    }
+}
+
+form.addEventListener('submit', formHandler);

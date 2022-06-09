7e149749374856688de7616205ed5fe4bfd0cf17
wilgru <william.gruszka@gmail.com>
2022-06-09 18:20:05 +1000

    changed API response

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -62,10 +62,10 @@ router.post('/signup', async (req, res) => {
 router.post('/logout', async (req, res) => {
     if (req.session.logged_in) {
         req.session.destroy(() => {
-            res.status(204).redirect('/');
+            res.status(204).end();
         })
     } else {
-        res.status(404).redirect('/');
+        res.status(404).end();
     }
 });
 
